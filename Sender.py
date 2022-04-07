2f90e8371b0e4a15e0903f68d92fd65d5e8d808d
Mayya Yashaswin (MS/EVV4-XC) <myy4kor@bosch.com>
2022-04-07 09:40:59 +0530

    Added Quality Parameters

--- Sender.py
+++ Sender.py
@@ -1,5 +1,4 @@
 import sys
-import time
 from Data_Generator import *
 
 limit_samples = 50
@@ -18,14 +17,12 @@ class sender:
 
     def consoleWrite(self):
 
-        sys.stdout.write(f'State of Charge(%)\tBatteryTemperature(C)\n')
+        sys.stdout.write(f'State of Charge(%) - Battery Temperature(C)\n')
         
         for sample in range(limit_samples):
             temparature_data = data_generator(BMS_temperature['min_value'], BMS_temperature['max_value']).parameter_value_generate()
             SOC_data = data_generator(BMS_SOC['min_value'], BMS_SOC['max_value']).parameter_value_generate()
-            sys.stdout.write(f'{SOC_data}\t\t\t{temparature_data}\n')
-            #print(f'{SOC_data}\t\t{temparature_data}')
-            #time.sleep(1)
+            sys.stdout.write(f'{SOC_data} - {temparature_data}\n')
 
 if __name__ == '__main__':
     sender().consoleWrite()
