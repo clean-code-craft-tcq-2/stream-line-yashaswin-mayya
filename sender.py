dbc9900de47860711a19e7a224611b28bb4fe282
Mayya Yashaswin (MS/EVV4-XC) <myy4kor@bosch.com>
2022-04-08 08:32:48 +0530

    Updated Code

--- sender.py
+++ sender.py
@@ -17,13 +17,13 @@ class Sender:
 
     def Get_ParametersData(self):
 
-        self.Write_To_Console(f'State of Charge(%)\t-\tBattery Temperature(°C)')
+        self.Write_To_Console(f'State of Charge(%), Battery Temperature(°C)')
         
         sampleCounter = 0
         while sampleCounter < Stream_Limit:
             temparature_data = Data_Generator().Parameter_Value_Generator(BMS_Temperature['min_value'], BMS_Temperature['max_value'])
             SOC_data = Data_Generator().Parameter_Value_Generator(BMS_SOC['min_value'], BMS_SOC['max_value'])
-            self.Write_To_Console(f'{SOC_data}\t-\t{temparature_data}')
+            self.Write_To_Console(f'{SOC_data}, {temparature_data}')
             sampleCounter+=1
             
         return sampleCounter
@@ -32,3 +32,5 @@ class Sender:
     def Write_To_Console(self, consoleMessage):
         sys.stdout.write(f'{consoleMessage}\n')
         return consoleMessage
+    
+Sender().Get_ParametersData()
