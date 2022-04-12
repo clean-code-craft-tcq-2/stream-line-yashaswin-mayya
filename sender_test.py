99a481c87316c7af4169912a5b4288e7f774db4c
Mayya Yashaswin (MS/EVV4-XC) <myy4kor@bosch.com>
2022-04-12 22:25:51 +0530

    Updated Code

--- sender_test.py
+++ sender_test.py
@@ -5,7 +5,7 @@ import unittest
 class Sender_Test(unittest.TestCase):
 
     def test_Parameter_Value_Generator_Digital(self):
-        #Parameter Value Generator is defined to generate random values within a range in order to simulate sensor data, hence no definite assert such as assertTrue 
+        #Parameter_Value_Generator_Digital is defined to generate random values within a range in order to simulate sensor data, hence no definite assert such as assertTrue 
         self.assertIsNotNone(Data_Generator().Parameter_Value_Generator_Digital(12))
         self.assertGreaterEqual(Data_Generator().Parameter_Value_Generator_Digital(12), 0)
         self.assertLessEqual(Data_Generator().Parameter_Value_Generator_Digital(12), 4096)
@@ -19,7 +19,7 @@ class Sender_Test(unittest.TestCase):
         self.assertEqual(Data_Generator().Digital_To_Analog_Converer(10, 511, 50, 150), 99.951)
 
     def test_Send_Parameter_Data(self):
-        pass
+        #Send_Parameter_Data is defined to generate random values within a range in order to simulate sensor data, hence no definite assert such as assertTrue
         self.assertIsNotNone(Data_Generator().Send_Parameter_Data(0, 50, 12))
         self.assertGreaterEqual(Data_Generator().Send_Parameter_Data(0, 50, 12), 0)
         self.assertLessEqual(Data_Generator().Send_Parameter_Data(0, 50, 12), 50)
@@ -27,6 +27,7 @@ class Sender_Test(unittest.TestCase):
 
     
     def test_Get_Parameter_To_Send(self):
+        #Test to ensure no. of samples sent by sender is equal to prescribed limit
         self.assertEqual(Sender().Get_ParametersData(), 50)
 
     def test_Write_To_Console(self):
