fa379ff1fbb5bc5f5b2bb0d36e7f17cffd330517
Mayya Yashaswin (MS/EVV4-XC) <myy4kor@bosch.com>
2022-04-22 15:50:22 +0530

    Updated Receiver

--- receiver.py
+++ receiver.py
@@ -1,32 +1,33 @@
+from asyncio.windows_events import NULL
 import sys
 
-
+samplesInMovingAverage = 5
 class Receiver:
 
     def getRawValuesFromConsole(self):
         consoleReadValues = []
         incomingStreamLines = sys.stdin.readlines()
         for streamValue in incomingStreamLines:
-            streamValue = streamValue.strip()
-            if streamValue == '\n':
+            streamValue = streamValue.strip('\n')
+            if not streamValue:
                 continue
             consoleReadValues.append(streamValue)
         return consoleReadValues
 
-    def processInput(self):
-        unprocessedInput = self.getRawValuesFromConsole()
+    def processInput(self, unprocessedInput):
         for index in range(len(unprocessedInput)):
             if 'Temperature' in unprocessedInput[index]:
                 temperatureDataStartIndex = index
             if 'SOC' in unprocessedInput[index]:
                 SOCDataStartIndex = index
-        processedTemperatureData = unprocessedInput[temperatureDataStartIndex+1:SOCDataStartIndex]
-        processedSOCData = unprocessedInput[SOCDataStartIndex+1:]
-        return processedTemperatureData, processedSOCData
+        processedTemperatureData = list(map(int, unprocessedInput[temperatureDataStartIndex+1:SOCDataStartIndex]))
+        processedSOCData = list(map(int, unprocessedInput[SOCDataStartIndex+1:]))
+        return (processedTemperatureData, processedSOCData)
 
-    def getMinAndMaxData(self):
+    def getMinAndMaxData(self, processedIncomingValues):
         
-        processedTemperatureData, processedSOCData = self.processInput()
+        processedTemperatureData = processedIncomingValues[0]
+        processedSOCData = processedIncomingValues[1]
         Temperature_Min = min(processedTemperatureData)
         Temperature_Max = max(processedTemperatureData)
         SOC_Min = min(processedSOCData)
@@ -35,19 +36,24 @@ class Receiver:
         self.printToConsole(f'\nMinimum Temperature is {Temperature_Min} and Maximum Temperature is {Temperature_Max}')
         self.printToConsole(f'\nMinimum SOC is {SOC_Min} and Maximum SOC is {SOC_Max}')
 
+        return [min(Temperature_Min,SOC_Min), max(Temperature_Max,SOC_Max)]
+
     
-    def getSimpleMovingAverage(self):
-        samplesInMovingAverage = 5
-        processedTemperatureData, processedSOCData = self.processInput()
+    def getSimpleMovingAverage(self, samplesInMovingAverage, processedIncomingValues):
+        processedTemperatureData = processedIncomingValues[0]
+        processedSOCData = processedIncomingValues[1]
 
         self.printToConsole(f'\nLast {samplesInMovingAverage} moving average for Temperature:')
-        for sample in self.calculateSimpleMovingAverage(samplesInMovingAverage, processedTemperatureData):
+        simpleMovingAverage_Temperature = self.calculateSimpleMovingAverage(samplesInMovingAverage, processedTemperatureData)
+        for sample in simpleMovingAverage_Temperature:
             self.printToConsole(sample)
         
-
         self.printToConsole(f'\nLast {samplesInMovingAverage} moving average for SOC:')
-        for sample in self.calculateSimpleMovingAverage(samplesInMovingAverage, processedSOCData):
+        simpleMovingAverage_SOC = self.calculateSimpleMovingAverage(samplesInMovingAverage, processedSOCData)
+        for sample in simpleMovingAverage_SOC:
             self.printToConsole(sample)
+
+        return (simpleMovingAverage_Temperature,simpleMovingAverage_SOC)
 
 
     def calculateSimpleMovingAverage(self, samplesInMovingAverage, listToProcess):
@@ -66,5 +72,17 @@ class Receiver:
         print(consoleMessage)
         return consoleMessage
 
-#Receiver().getMinAndMaxData()
-#Receiver().getSimpleMovingAverage()
+    def main(self):
+        isExecutionComplete = False
+        if not isExecutionComplete:
+           rawIncomingValues =  self.getRawValuesFromConsole()
+           processedIncomingValues = self.processInput(rawIncomingValues)
+           self.getMinAndMaxData(processedIncomingValues)
+           self.getSimpleMovingAverage(samplesInMovingAverage, processedIncomingValues)
+           isExecutionComplete = True
+        return isExecutionComplete
+
+
+
+
+#Receiver().main()
--- receiver_test.py
+++ receiver_test.py
@@ -1,7 +1,24 @@
 from receiver import *
 import unittest
+import sys
 
 class Receiver_Test(unittest.TestCase):
+
+    def test_getRawValuesFromConsole(self):
+        sys.stdout.writelines("5\n8\n2\n10\n37\n55")
+        self.assertEqual(Receiver().getRawValuesFromConsole(), [5,8,2,10,37,55])
+    
+    def test_processInput(self):
+        self.assertEqual(Receiver().processInput(['Temperature Data','1','3','5','SOC Data','7','11','18']), ([1,3,5],[7,11,18]))
+        self.assertIsInstance(Receiver().processInput(['Temperature Data','1','3','5','SOC Data','7','11','18']), tuple)
+
+    def test_getMinAndMaxData(self):
+        self.assertEqual(Receiver().getMinAndMaxData(([1,3,5],[7,11,18])), [1,18])
+        self.assertIsInstance(Receiver().getMinAndMaxData(([1,3,5],[7,11,18])), list)
+
+    def test_getSimpleMovingAverage(self):
+        self.assertEqual(Receiver().getSimpleMovingAverage(3, ([1,7,5,7,8,4],[8,13,21,43,67,12])), (['-','-',4.33,6.33,6.67,6.33],['-','-',14,25.67,43.67,40.67]))
+        self.assertIsInstance(Receiver().getSimpleMovingAverage(3, ([1,7,5,7,8,4],[8,13,21,43,67,12])), (['-','-',4.33,6.33,6.67,6.33],['-','-',14,25.67,43.67,40.67]), tuple)
 
     def test_calculateSimpleMovingAverage(self):
         self.assertEqual(Receiver().calculateSimpleMovingAverage(3, [1,7,5,7,8,4]), ['-','-',4.33,6.33,6.67,6.33])
@@ -11,5 +28,8 @@ class Receiver_Test(unittest.TestCase):
     def test_printToConsole(self):
         self.assertEqual(Receiver().printToConsole('Test Data'), 'Test Data')
         self.assertIsInstance(Receiver().printToConsole('Test Data'), str)
+
+    def test_main(self):
+        self.assertTrue(Receiver().main())
 
 unittest.main()
