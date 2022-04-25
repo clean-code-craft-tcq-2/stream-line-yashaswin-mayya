b1c205c9b91ac8460b9e5737bff805bb88190fde
yashaswin-mayya <91110406+yashaswin-mayya@users.noreply.github.com>
2022-04-25 06:35:06 +0530

    Update sender.py

--- sender.py
+++ sender.py
@@ -18,30 +18,16 @@ BMS_SOC = {
 class Sender:
 
     def Get_ParametersData(self):
+        
+        
+        self.Write_To_Console(f'State of Charge(%), Battery Temperature(°C)')
 
-        self.Write_To_Console(f'Battery Temperature(°C)')
         sampleCounter = 0
         while sampleCounter < Stream_Limit:
             temparature_data = Data_Generator().Send_Parameter_Data(BMS_Temperature['min_value'], BMS_Temperature['max_value'], Sensor_Bits)
-            #SOC_data = Data_Generator().Send_Parameter_Data(BMS_SOC['min_value'], BMS_SOC['max_value'], Sensor_Bits)
-            self.Write_To_Console(f'{temparature_data}')
-            sampleCounter+=1
-        self.Write_To_Console(f'\nBattery SOC(°C)')
-        sampleCounter = 0
-        while sampleCounter < Stream_Limit:
-            #temparature_data = Data_Generator().Send_Parameter_Data(BMS_Temperature['min_value'], BMS_Temperature['max_value'], Sensor_Bits)
             SOC_data = Data_Generator().Send_Parameter_Data(BMS_SOC['min_value'], BMS_SOC['max_value'], Sensor_Bits)
-            self.Write_To_Console(f'{SOC_data}')
+            self.Write_To_Console(f'{SOC_data}, {temparature_data}')
             sampleCounter+=1
-
-        # self.Write_To_Console(f'State of Charge(%), Battery Temperature(°C)')
-
-        # sampleCounter = 0
-        # while sampleCounter < Stream_Limit:
-        #     temparature_data = Data_Generator().Send_Parameter_Data(BMS_Temperature['min_value'], BMS_Temperature['max_value'], Sensor_Bits)
-        #     SOC_data = Data_Generator().Send_Parameter_Data(BMS_SOC['min_value'], BMS_SOC['max_value'], Sensor_Bits)
-        #     self.Write_To_Console(f'{SOC_data}, {temparature_data}')
-        #     sampleCounter+=1
 
         return sampleCounter
         
