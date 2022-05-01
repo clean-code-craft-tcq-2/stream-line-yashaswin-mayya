1cc139f0e579b6934ed77f847f81927c5e3e11fc
cheral-bhati <91013880+cheral-bhati@users.noreply.github.com>
2022-05-01 23:58:40 +0530

    Update receiver_test.cpp

--- Receiver/receiver_test.cpp
+++ Receiver/receiver_test.cpp
@@ -1,1 +1,19 @@
+#define CATCH_CONFIG_MAIN
+#include "test/catch.hpp"
+#include "receiver.h"
 
+TEST_CASE("Check whether the data is read from console") 
+{
+  float SOC[NO_OF_READINGS] = {0};
+  float Temperature[NO_OF_READINGS] = {0};
+
+  ReceiverData(SOC,Temperature);
+  float expectedoutput[3][2] = {{79.326,9.747}, {47.868,13.747},{78.344, 38.571}};
+  for(int i=0;i<3;i++)
+  {
+    REQUIRE(SOC[i] == expectedoutput[i][1]);
+    REQUIRE(Temperature[i] == expectedoutput[i][0]);
+  }
+}
+  
+  
