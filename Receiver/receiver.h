5db521e4f33969e8049417ffa3d08ca7552eceb9
cheral-bhati <91013880+cheral-bhati@users.noreply.github.com>
2022-05-01 21:03:38 +0530

    Create receiver.h

---
+++ Receiver/receiver.h
@@ -0,0 +1,8 @@
+#define NO_OF_READINGS 50
+
+float CalculateSMA(float inputData[]);
+float CalculateMaxValue(float inputData[]);
+float CalculateMinValue(float inputData[]);
+
+void ReadParametersfromConsole(float* SOC, float* Temperature);
+void PrintComputedData(float *BMSParameter, float MaxValue, float Minvalue, float SMA);
