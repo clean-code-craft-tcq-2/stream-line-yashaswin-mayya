30249e60b3892155101dc654dcddf873b5ce69eb
cheral-bhati <91013880+cheral-bhati@users.noreply.github.com>
2022-05-02 00:11:38 +0530

    Update receiver.h

--- Receiver/receiver.h
+++ Receiver/receiver.h
@@ -6,3 +6,4 @@ float CalculateMinValue(float inputData[]);
 
 void ReadParametersfromConsole(float* SOC, float* Temperature);
 void PrintComputedData(float *BMSParameter, float MaxValue, float Minvalue, float SMA);
+void ReceiverData(float* SOC, float* Temperature);
