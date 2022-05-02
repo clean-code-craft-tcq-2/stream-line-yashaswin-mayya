ff27f8ee24433e52d34c82f356f6eeac5d1eb942
cheral-bhati <91013880+cheral-bhati@users.noreply.github.com>
2022-05-02 09:32:42 +0530

    Delete Receiver directory

--- Receiver/receiver.h
+++
@@ -1,9 +0,0 @@
-#define NO_OF_READINGS 50
-
-float CalculateSMA(float inputData[]);
-float CalculateMaxValue(float inputData[]);
-float CalculateMinValue(float inputData[]);
-
-void ReadParametersfromConsole(float* SOC, float* Temperature);
-void PrintComputedData(float *BMSParameter, float MaxValue, float Minvalue, float SMA);
-void ReceiverData(float* SOC, float* Temperature);
