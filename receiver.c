cdddd45fe8aa1a291b1d1d1a48d48a455a102e54
cheral-bhati <91013880+cheral-bhati@users.noreply.github.com>
2022-05-03 13:44:15 +0530

    Update receiver.c

--- receiver.c
+++ receiver.c
@@ -5,7 +5,7 @@ void ReadParametersfromConsole(float* SOC, float* Temperature)
 {
   char ReadString[20];
   
-  for(int paramindex=1; paramindex<NO_OF_READINGS; paramindex++)
+  for(int paramindex=0; paramindex<NO_OF_READINGS; paramindex++)
     {
        if(scanf("%20s", ReadString) == EOF) 
        {
