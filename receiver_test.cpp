87717b3e53205135cd29a8faef462b2e23226cde
cheral-bhati <91013880+cheral-bhati@users.noreply.github.com>
2022-05-04 00:42:41 +0530

    Update receiver_test.cpp

--- receiver_test.cpp
+++ receiver_test.cpp
@@ -2,11 +2,7 @@
 #include "test/catch.hpp"
 #include "receiver.h"
 
-TEST_CASE("Test to calculate maximum value in an array")
+TEST_CASE("Process sensor input and output in console")
 {
-    float inputarray[5] = {1.05, 80.2, 48, 17.66, 0.001};
-    float MaxValue = CalculateMaxValue(inputarray);
-
-    REQUIRE(MaxValue == 80.2f);
+  REQUIRE(ReceiverData() == 1);
 }
-
