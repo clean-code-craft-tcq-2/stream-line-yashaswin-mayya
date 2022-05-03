ead0796541f7bc29134e2194a78cd3fa1478a2a5
cheral-bhati <91013880+cheral-bhati@users.noreply.github.com>
2022-05-04 00:59:09 +0530

    Update receiver_test.cpp

--- receiver_test.cpp
+++ receiver_test.cpp
@@ -2,4 +2,12 @@
 #include "test/catch.hpp"
 #include "receiver.h"
 
-
+//TEST_CASE("Tests to check whether sensor data is read from console") 
+//{
+  float Temperature[NO_OF_READINGS] = {0};
+  float SOC[NO_OF_READINGS] = {0};
+  //float ChargeRate[NO_OF_READINGS] = {0};
+  //float Current_MaxValue, Current_MinValue, Current_SMAValue, expectedMaxValue, expectedMinValue, expectedSMAValue;
+  
+  ReceiverData(SOC,Temperature);
+ 
