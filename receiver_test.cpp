625119cd25a0105e8a21945f91b519139804ba3e
cheral-bhati <91013880+cheral-bhati@users.noreply.github.com>
2022-05-04 00:01:16 +0530

    Update receiver_test.cpp

--- receiver_test.cpp
+++ receiver_test.cpp
@@ -2,3 +2,11 @@
 #include "test/catch.hpp"
 #include "receiver.h"
 
+TEST_CASE("Test to calculate maximum value in an array")
+{
+    float inputarray[5] = {1.05, 80.2, 48, 17.66, 0.001};
+    float MaxValue = CalculateMaxValue(inputarray);
+
+    REQUIRE(MaxValue == 80.2);
+}
+
